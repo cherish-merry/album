8e2f8e982578b00559512d6116c24b690e8e43a2
bingchuanfeiyu <2861124797@qq.com>
2022-06-15 17:06:02 +0800

    add photos

--- tool.py
+++ tool.py
@@ -2,22 +2,11 @@
 from PIL import Image
 import shutil
 import os
-import sys
 import json
 from datetime import datetime
 
 
-# from ImageProcess import Graphics
-
 class Graphics:
-    '''图片处理类
-    
-    参数
-    -------
-    infile: 输入文件路径
-    outfile: 输出文件路径
-    '''
-
     def __init__(self, infile, outfile):
         self.infile = infile
         self.outfile = outfile
@@ -61,47 +50,26 @@ class Graphics:
         if q == 100:
             shutil.copy(self.infile, self.outfile)
 
-    def cut_by_ratio(self):
-        """按照图片长宽进行分割
-        
-        ------------
-        取中间的部分，裁剪成正方形
-        """
+    def cut_compress(self):
         im = Image.open(self.infile)
         (x, y) = im.size
         if x > y:
             region = (int(x / 2 - y / 2), 0, int(x / 2 + y / 2), y)
-            # 裁切图片
-            crop_img = im.crop(region)
-            # 保存裁切后的图片
-            crop_img.save(self.outfile)
-        elif x < y:
+        else:
             region = (0, int(y / 2 - x / 2), x, int(y / 2 + x / 2))
-            # 裁切图片
-            crop_img = im.crop(region)
-            # 保存裁切后的图片
-            crop_img.save(self.outfile)
+        # 裁切图片
+        crop_img = im.crop(region)
+        w, h = crop_img.size
+        crop_img.thumbnail((int(w / SIZE_more_small_small), int(h / SIZE_more_small_small)))
+        crop_img.save(self.outfile)
 
-        # 定义压缩比，数值越大，压缩越小
 
-
+SRC_DIR = "./photos/"
+DES_DIR = "./min_photos/"
 SIZE_normal = 1.0
 SIZE_small = 1.5
 SIZE_more_small = 2.0
 SIZE_more_small_small = 3.0
-
-
-def make_directory(directory):
-    """创建目录"""
-    os.makedirs(directory)
-
-
-def directory_exists(directory):
-    """判断目录是否存在"""
-    if os.path.exists(directory):
-        return True
-    else:
-        return False
 
 
 def list_img_file(directory):
@@ -109,66 +77,14 @@ def list_img_file(directory):
     old_list = os.listdir(directory)
     new_list = []
     for filename in old_list:
-        if not filename.startswith("._"):
-            name, fileformat = filename.split(".")
-            if fileformat.lower() == "jpg" or fileformat.lower() == "png" or fileformat.lower() == "gif":
-                new_list.append(filename)
+        name, formant = filename.split(".")
+        if formant.lower() == "jpg" or formant.lower() == "png" or formant.lower() == "gif":
+            new_list.append(filename)
     return new_list
 
 
-def print_help():
-    print("""
-    This program helps compress many image files
-    you can choose which scale you want to compress your img(jpg/png/etc)
-    1) normal compress(4M to 1M around)
-    2) small compress(4M to 500K around)
-    3) smaller compress(4M to 300K around)
-    """)
-
-
-def compress(choose, des_dir, src_dir, file_list):
-    """压缩算法，img.thumbnail对图片进行压缩，
-    
-    参数
-    -----------
-    choose: str
-            选择压缩的比例，有4个选项，越大压缩后的图片越小
-    """
-    if choose == '1':
-        scale = SIZE_normal
-    if choose == '2':
-        scale = SIZE_small
-    if choose == '3':
-        scale = SIZE_more_small
-    if choose == '4':
-        scale = SIZE_more_small_small
-    for infile in file_list:
-        img = Image.open(src_dir + infile)
-        # size_of_file = os.path.getsize(infile)
-        w, h = img.size
-        img.thumbnail((int(w / scale), int(h / scale)))
-        img.save(des_dir + infile)
-
-
-def compress_photo():
-    src_dir, des_dir = "photos/", "min_photos/"
-
-    file_list_src = list_img_file(src_dir)
-
-    file_list_des = list_img_file(des_dir)
-
-    '''如果已经压缩了，就不再压缩'''
-    for i in range(len(file_list_des)):
-        if file_list_des[i] in file_list_src:
-            file_list_src.remove(file_list_des[i])
-    compress('1', des_dir, src_dir, file_list_src)
-
-
 def handle_photo():
-    ''' 根据图片的文件名处理成需要的json格式的数据最后将data.json文件存到博客的source/photos文件夹下'''
-
-    src_dir, des_dir = "photos/", "min_photos/"
-    file_list = list_img_file(src_dir)
+    file_list = list_img_file(SRC_DIR)
     list_info = []
     for i in range(len(file_list)):
         filename = file_list[i]
@@ -205,19 +121,12 @@ def handle_photo():
         json.dump(final_dict, fp)
 
 
-def cut_photo():
-    """裁剪算法
-    ----------
-    调用Graphics类中的裁剪算法，将src_dir目录下的文件进行裁剪（裁剪成正方形）
-    """
-    src_dir = "photos/"
-    if directory_exists(src_dir):
-        file_list = list_img_file(src_dir)
-        if file_list:
-            print_help()
-            for infile in file_list:
-                Image.open(src_dir + infile)
-                Graphics(infile=src_dir + infile, outfile=src_dir + infile).cut_by_ratio()
+def cut_compress():
+    src_file_list = list_img_file(SRC_DIR)
+    des_file_list = list_img_file(DES_DIR)
+    for i in range(len(src_file_list)):
+        if src_file_list[i] not in des_file_list:
+            Graphics(infile=SRC_DIR + src_file_list[i], outfile=DES_DIR + src_file_list[i]).cut_compress()
 
 
 def git_operation():
@@ -226,7 +135,6 @@ def git_operation():
     os.system('git push origin master')
 
 
-# cut_photo()  # 裁剪图片，裁剪成正方形，去中间部分
-compress_photo()  # 压缩图片，并保存到mini_photos文件夹下
+cut_compress()  # 裁剪图片，裁剪成正方形，去中间部分
 git_operation()  # 提交到github仓库
 handle_photo()  # 将文件处理成json格式，存到博客仓库中
